bbc219d387bab7407884dfe3394bede6f255a027
Tim Yates <tim.yates@gmail.com>
2022-11-23 16:08:59 +0000

    Define micronaut-test version and spock

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,17 @@ pluginManagement {
         mavenCentral()
     }
 }
-plugins {
-    id 'io.micronaut.build.shared.settings' version '6.1.0'
-}
-rootProject.name = 'hibernate-validator-parent'
 
-include 'hibernate-validator-bom'
-include 'hibernate-validator'
-micronautBuild {
+plugins {
+    id("io.micronaut.build.shared.settings") version "6.1.0"
+}
+
+rootProject.name = "hibernate-validator-parent"
+
+include("hibernate-validator-bom")
+include("hibernate-validator")
+
+configure<io.micronaut.build.MicronautBuildSettingsExtension> {
     // Required for 4.0.0-SNAPSHOT, can be removed after
     addSnapshotRepository()
     importMicronautCatalog()
