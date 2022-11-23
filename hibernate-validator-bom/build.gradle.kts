24059e257dc6f3eb713c19b5b2b3ce48e41d0155
Sergio del Amo <sergio.delamo@softamo.com>
2022-11-23 16:42:19 +0100

    Update build.gradle.kts

--- hibernate-validator-bom/build.gradle.kts
+++ hibernate-validator-bom/build.gradle.kts
@@ -3,9 +3,3 @@ import org.gradle.internal.execution.caching.CachingState.enabled
 plugins {
     id("io.micronaut.build.internal.bom")
 }
-
-micronautBuild {
-    binaryCompatibility {
-        enabled.set(false)
-    }
-}
