354a23ebf1ef6a04bcc8e29d19b5ce5f15167e4f
Sergio del Amo <sergio.delamo@softamo.com>
2022-11-24 06:29:20 +0100

    enable binary compatiblity (#264)

--- hibernate-validator-bom/build.gradle.kts
+++ hibernate-validator-bom/build.gradle.kts
@@ -1,11 +1,3 @@
-import org.gradle.internal.execution.caching.CachingState.enabled
-
 plugins {
     id("io.micronaut.build.internal.bom")
-}
-
-micronautBuild {
-    binaryCompatibility {
-        enabled.set(false)
-    }
 }
