3ff3975ee46dc804e9d963054e9674b10b5a1490
Sergio del Amo <sergio.delamo@softamo.com>
2023-07-19 09:39:00 +0200

    Use Kotlin DSL

---
+++ hibernate-validator/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("io.micronaut.build.internal.hibernate-validator-module")
+}
+
+dependencies {
+    annotationProcessor(mn.micronaut.graal)
+    annotationProcessor(mn.micronaut.inject.java)
+    compileOnly(mn.micronaut.router)
+    implementation(libs.hibernate.validator)
+    implementation(mn.micronaut.inject)
+    implementation(mnValidation.micronaut.validation)
+
+    testAnnotationProcessor(mnValidation.micronaut.validation.processor)
+    testCompileOnly(mnValidation.micronaut.validation.processor)
+    testAnnotationProcessor(mn.micronaut.inject.java)
+    testCompileOnly(mn.micronaut.inject.groovy)
+    testImplementation(mn.micronaut.http.server.netty)
+    testRuntimeOnly(mn.snakeyaml)
+    testImplementation(mnSerde.micronaut.serde.jackson)
+}
